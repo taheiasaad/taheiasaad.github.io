d94a21870343eabbcc9409fd723b0fd57899bba2
taheiasaad <taheiasaad@gmail.com>
2025-09-05 22:50:16 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const text = "I'm Taheia Saad";
+  const typingElement = document.querySelector(".typing");
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      typingElement.textContent += text.charAt(index);
+      index++;
+      setTimeout(type, 120);
+    }
+  }
+
+  type();
+});
